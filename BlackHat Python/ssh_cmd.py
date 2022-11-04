7649a6dc5b9f3595f1da1fddc0f5e47bc7a351df
sxsm <thesgadyal@gmail.com>
2022-11-05 00:23:00 +0530

    added ssh tunneling

---
+++ BlackHat Python/ssh_cmd.py
@@ -0,0 +1,26 @@
+import paramiko
+
+def ssh_command(ip, port, user, passwd, cmd):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port=port, username=user, password=passwd)
+
+    _, stdout, stderr = client.exec_command(cmd)
+    output = stdout.readlines() + stderr.readlines()
+    if output:
+        print('--- OUTPUT ---')
+        for line in output:
+            print(line.strip())
+
+if __name__ == "__main__":
+    import getpass
+    user = input("USERNAME: ")
+    password = getpass.getpass()
+
+    ip = input("ENTER sever IP: ") or "192.168.1.203"
+    port = input("Enter port or <CR>: ") or 2222
+    cmd = input("Enter command or <CR>: ") or 'id'
+    ssh_command(ip, port, user, password, cmd)
+    
+
+
